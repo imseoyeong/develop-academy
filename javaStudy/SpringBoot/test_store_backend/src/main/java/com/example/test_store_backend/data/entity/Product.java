9e7d6150eb3c923413151c2744b66907fd555f05
imseoyeong <fearless40@naver.com>
2025-05-19 10:17:01 +0900

    update

--- javaStudy/SpringBoot/test_store_backend/src/main/java/com/example/test_store_backend/data/entity/Product.java
+++ javaStudy/SpringBoot/test_store_backend/src/main/java/com/example/test_store_backend/data/entity/Product.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 //entity 클래스
 @Entity
 @Table(name = "producttbl")
@@ -20,4 +22,6 @@ public class Product {
     private String title;
     private Integer price;
     private String imagesrc;
+    private LocalDateTime created; // 생성 또는 수정된 시간을 저장
+    private String description; // 생성인지 수정인지 나타내는 설명글
 }
