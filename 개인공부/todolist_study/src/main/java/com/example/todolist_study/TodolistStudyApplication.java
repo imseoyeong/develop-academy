2e57c0d7640b9ebc4a01cd1cba0bfcfac16bfed9
imseoyeong <fearless40@naver.com>
2025-06-18 21:19:08 +0900

    /

---
+++ 개인공부/todolist_study/src/main/java/com/example/todolist_study/TodolistStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolist_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistStudyApplication.class, args);
+    }
+
+}
